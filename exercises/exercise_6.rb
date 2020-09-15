866c3100646e9f07fa28d6df0d29d5aa0e619a55
Sam <super8989@gmail.com>
2020-09-14 23:54:56 -0400

    Add data into employees

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+# Update store1 name back to Burnaby
+burnaby = Store.find_by(id: 1)
+burnaby.name = 'Burnaby'
+burnaby.save
+
+# Add some data into employees
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Captain", last_name: "America", hourly_rate: 100)
+@store2.employees.create(first_name: "Iron", last_name: "Man", hourly_rate: 300)
+@store2.employees.create(first_name: "Black", last_name: "Widow", hourly_rate: 200)
