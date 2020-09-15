37d540aaedc9d56bb4f1990fd9ecd40a2fe1e136
Sam <super8989@gmail.com>
2020-09-15 00:54:35 -0400

    Validations for Stores and Employees

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,13 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+# Ask the user for a store name (store it in a variable)
+puts "Provide a store name:"
+response = gets.chomp
+
+# Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+new_store = Store.create(name: "#{response}")
+
+# Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+new_store.errors.messages.each { |x| puts x }
